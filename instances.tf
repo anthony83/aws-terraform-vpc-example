5251639934cfc61866d25f9122bd13408c3dc7d4
anthony83 <anthony@nard1.net>
2020-08-28 16:39:23 +1000

    fixed vpc

--- instances.tf
+++ instances.tf
@@ -1,10 +1,25 @@
 resource "aws_instance" "webservers" {
 	#count = "${length(var.subnets_cidr)}"
-  count = 0
+  count = 2
 	ami = "${var.webservers_ami}"
 	instance_type = "${var.instance_type}"
 	security_groups = ["${aws_security_group.webservers.id}"]
 	subnet_id = "${element(aws_subnet.public.*.id,count.index)}"
+	user_data = "${file("install_httpd.sh")}"
+
+  tags = {
+  	  Name = "Server-${count.index}"
+  	}
+
+}
+
+resource "aws_instance" "webservers_private" {
+	#count = "${length(var.subnets_cidr)}"
+  count = 2
+	ami = "${var.webservers_ami}"
+	instance_type = "${var.instance_type}"
+	security_groups = ["${aws_security_group.webservers.id}"]
+	subnet_id = "${element(aws_subnet.private.*.id,count.index)}"
 	user_data = "${file("install_httpd.sh")}"
 
   tags = {
