18c0e17cf023e18f5e70480790ea0292ac2906ee
anthony83 <anthony@nard1.net>
2020-08-28 13:58:56 +1000

    first commit

---
+++ instances.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "webservers" {
+	#count = "${length(var.subnets_cidr)}"
+  count = 1
+	ami = "${var.webservers_ami}"
+	instance_type = "${var.instance_type}"
+	security_groups = ["${aws_security_group.webservers.id}"]
+	subnet_id = "${element(aws_subnet.public.*.id,count.index)}"
+	user_data = "${file("install_httpd.sh")}"
+
+  tags = {
+  	  Name = "Server-${count.index}"
+  	}
+
+}
