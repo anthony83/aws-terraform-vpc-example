b6f4e4409a87fd92e761209292a264b8fc9fe958
anthony83 <anthony@nard1.net>
2020-08-28 14:15:06 +1000

    first commit

--- instances.tf
+++ instances.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "webservers" {
 	#count = "${length(var.subnets_cidr)}"
-  count = 2
+  count = 0
 	ami = "${var.webservers_ami}"
 	instance_type = "${var.instance_type}"
 	security_groups = ["${aws_security_group.webservers.id}"]
